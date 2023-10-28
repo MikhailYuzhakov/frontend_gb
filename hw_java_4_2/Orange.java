a690bae23da0dc499a24bd79bbdd24a708d55923
Mikhail Yuzhakov <miha.yuzhakov@gmail.com>
2023-10-28 14:26:55 +0300

    домашнее задание 4(лекционное)

---
+++ hw_java_4_2/Orange.java
@@ -0,0 +1,16 @@
+public class Orange {
+    private Juice juice;
+    public Orange() {
+        this.juice = new Juice();
+    }
+    public void squeezeJuice() {
+        System.out.println("Squeeze juice...");
+        juice.flow();
+    }
+
+    private class Juice {
+        public void flow() {
+            System.out.println("Juice dripped...");
+        }
+    }
+}
